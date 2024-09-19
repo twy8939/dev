e9362373878e3bb89b6c5be537535d2b87587b24
twy8939 <twy8939@naver.com>
2024-09-19 22:41:26 +0900

    feat : Learn Promise.race

--- packages/dev/src/concurrent.ts
+++ packages/dev/src/concurrent.ts
@@ -2,8 +2,25 @@ function delay<T>(time: number, value: T): Promise<T> {
   return new Promise((res) => setTimeout(() => res(value), time));
 }
 
+interface File {
+  name: string;
+  body: string;
+  size: number;
+}
+
+function getFile(name: string): Promise<File> {
+  return delay(1500, { name, body: "...", size: 100 });
+}
+
 export async function concurrent() {
-  console.log(await delay(1000, "hello"));
-  console.log(await delay(2000, "world"));
-  console.log(await delay(3000, "!!!"));
+  const file = getFile("file1.png");
+
+  const result = await Promise.race([file, delay(1000, "timeout")]);
+
+  if (result === "timeout") {
+    console.log("로딩");
+    console.log(await file);
+  } else {
+    console.log("로딩 없이 표시");
+  }
 }
